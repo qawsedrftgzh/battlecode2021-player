ee0a2ba060567601186cc7960192ac58e536ea34
SkyNet214 <niklas20066@gmail.com>
2021-01-08 01:34:57 +0100

    added tryMove() function to Unit.java; added a few things to Util.java that belong to util

--- Unit.java
+++ Unit.java
@@ -11,4 +11,21 @@ public class Unit extends Robot {
         super.takeTurn();
     }
 
+    /**
+     * Attempts to move in a given direction.
+     *
+     * @param dir The intended direction of movement
+     * @return true if a move was performed
+     * @throws GameActionException
+     */
+    public boolean tryMove(Direction dir) throws GameActionException {
+        System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
