10f8563280cdd316ad99040143297793c2dab68a
SkyNet214 <niklas20066@gmail.com>
2021-01-09 00:25:16 +0100

    added attackProtocol() function to Politician.java

--- Politician.java
+++ Politician.java
@@ -2,10 +2,13 @@ package battlecode2021;
 import battlecode.common.*;
 
 public class Politician extends Unit {
-    MapLocation born = rc.getLocation();
+    Team enemy = rc.getTeam().opponent();
+    int actionRadius = rc.getType().actionRadiusSquared;
+
 
     public Politician(RobotController r) {
         super(r);
+        MapLocation born = rc.getLocation();
     }
 
     public void takeTurn() throws GameActionException {
@@ -14,30 +17,37 @@ public class Politician extends Unit {
         if (rc.getRoundNum() <= 50) {
             rc.setFlag(999999);
         }
-        Team enemy = rc.getTeam().opponent();
 
-        int actionRadius = rc.getType().actionRadiusSquared;
-        RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
-        for (RobotInfo bot : attackable) {
-            if (bot.type == RobotType.ENLIGHTENMENT_CENTER && rc.canEmpower(rc.getLocation().distanceSquaredTo(bot.location))) {
-                rc.empower(rc.getLocation().distanceSquaredTo(bot.location));
-            }
-        }
-        if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
-            rc.empower(actionRadius);
-            return;
-        }
-        boolean good = tryMove(Util.randomDirection());
-        if (good == false){
-            for (Direction dir : Util.directions) {
-                if (tryMove(dir) == true) {
-                    good = true;
-                    break;
+        if (!attackProtocol()) {
+            boolean good = tryMove(Util.randomDirection());
+            if (good == false) {
+                for (Direction dir : Util.directions) {
+                    if (tryMove(dir) == true) {
+                        good = true;
+                        break;
+                    }
                 }
             }
-        } if (good == false && rc.canEmpower(actionRadius)) { //there is no space anywhere; let's hold a speech
-            rc.empower(actionRadius);
+            if (good == false && rc.canEmpower(actionRadius)) { //there is no space anywhere; let's hold a speech
+                rc.empower(actionRadius);
+            }
+            //flags.main();
         }
-        flags.main();
+    }
+
+    boolean attackProtocol() throws GameActionException {
+        RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
+        if (attackable.length != 0 && rc.isReady()) {
+            for (RobotInfo bot : attackable) {
+                if (bot.type == RobotType.ENLIGHTENMENT_CENTER && rc.canEmpower(rc.getLocation().distanceSquaredTo(bot.location))) {
+                    rc.empower(rc.getLocation().distanceSquaredTo(bot.location));
+                    return true;
+                }
+            }
+        } else if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
+            rc.empower(actionRadius);
+            return true;
+        }
+        return false;
     }
 }
