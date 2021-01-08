682958a98703161f4931ac02927a2a476d2bb486
SkyNet214 <niklas20066@gmail.com>
2021-01-09 00:31:02 +0100

    added a object of Flags to use the Flags class

--- Robot.java
+++ Robot.java
@@ -3,10 +3,12 @@ import battlecode.common.*;
 
 public class Robot {
     RobotController rc;
+    Flags flags;
     int turnCount = 0;
 
     public Robot(RobotController r) {
         this.rc = r;
+        flags = new Flags(rc);
     }
 
     public void takeTurn() throws GameActionException {
