4e077c0998797738b4dbc036ba8452de6d39fef9
SkyNet214 <niklas20066@gmail.com>
2021-01-11 21:26:21 +0100

    added RobotType type, MapLocation enemyECloc, RobotInfo[] nearbyTeam, nearbyEnemys, attackable, int actionRadius

--- Robot.java
+++ Robot.java
@@ -1,20 +1,31 @@
 package battlecode2021;
 import battlecode.common.*;
 
+
 public class Robot {
     RobotController rc;
     Flags flags;
     int turnCount = 0;
     Team enemy, team;
+    RobotType type;
+    MapLocation enemyECloc;
+    RobotInfo[] nearbyTeam, nearbyEnemys, attackable;
+    int actionRadius;
 
     public Robot(RobotController r) {
         this.rc = r;
         flags = new Flags(rc);
-        enemy = rc.getTeam().opponent();
         team = rc.getTeam();
+        enemy = team.opponent();
+        type = rc.getType();
+        actionRadius = rc.getType().actionRadiusSquared;
+
     }
 
     public void takeTurn() throws GameActionException {
         turnCount += 1;
+        nearbyTeam = rc.senseNearbyRobots(type.sensorRadiusSquared, team);
+        nearbyEnemys = rc.senseNearbyRobots(type.sensorRadiusSquared, enemy);
+        attackable = rc.senseNearbyRobots(actionRadius, enemy);
     }
 }
