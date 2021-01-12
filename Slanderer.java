61cca8ad50e47c57cebb559512d7c1bf6d94e132
SkyNet214 <niklas20066@gmail.com>
2021-01-12 01:12:39 +0100

    minor changes

--- Slanderer.java
+++ Slanderer.java
@@ -6,16 +6,16 @@ public class Slanderer extends Unit {
     public Slanderer(RobotController r) {
         super(r);
     }
-    Team enemy = rc.getTeam().opponent();
 
     public void takeTurn() throws GameActionException {
         RobotInfo[] bots = rc.senseNearbyRobots(RobotType.SLANDERER.detectionRadiusSquared,enemy);
         for (RobotInfo bot : bots) {
             if (bot.type == RobotType.MUCKRAKER){
-                nav.tryMove(bot.location.directionTo(rc.getLocation()));
+                nav.runaway(bot.location);
+
             }
         } nav.tryMove(Util.randomDirection());
-        flags.main();
+
     }
 }
 
