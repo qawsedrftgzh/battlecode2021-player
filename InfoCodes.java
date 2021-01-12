508d7e554becd6ad7b6b0ce98b62b6a1620a513c
SkyNet214 <niklas20066@gmail.com>
2021-01-12 01:08:06 +0100

    added 2 info codes

--- InfoCodes.java
+++ InfoCodes.java
@@ -1,2 +1,6 @@
-package battlecode2021;public class InfoCode {
+package battlecode2021;
+
+public class InfoCodes {
+    public static final int STARTATTACK = 0;
+    public static final int STOPATTACK = 1;
 }
