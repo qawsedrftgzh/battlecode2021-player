9fc477321a8f4645690afc9ef50dc541c24de2c7
SkyNet214 <niklas20066@gmail.com>
2021-01-12 01:05:16 +0100

    edited takeTurn(); added updateFlag()

--- Politician.java
+++ Politician.java
@@ -1,6 +1,8 @@
 package battlecode2021;
 import battlecode.common.*;
 
+import javax.sound.sampled.Line;
+
 public class Politician extends Unit {
     boolean superpol = false; //if the plotician has more then 200 influence
     public Politician(RobotController r) {
@@ -12,7 +14,14 @@ public class Politician extends Unit {
 
     public void takeTurn() throws GameActionException {
         super.takeTurn();
-        flags.main();
+        if (enemyECloc != null) {
+            attack(enemyECloc, 1);
+        } else if (!nav.runaway(ECloc)) {
+            if (!idleMovement() && rc.canEmpower(actionRadius)) { //there is no space anywhere; let's hold a speech
+                rc.empower(actionRadius);
+            }
+        }
+        /**
         if (!attackProtocol()) {
             if (rc.getLocation().distanceSquaredTo(ECloc) < 50) {
                 nav.scout(ECloc);
@@ -21,43 +30,21 @@ public class Politician extends Unit {
                     rc.empower(actionRadius);
                 }
             }
-        }
-    }
-
-    public void takeTurn2() throws GameActionException {
-        super.takeTurn();
-
-
-        if (!attackProtocol()) {
-            if (!idleMovement() && rc.canEmpower(actionRadius)) { //there is no space anywhere; let's hold a speech
-                rc.empower(actionRadius);
-            }
-            //flags.main();
-        }
+        } **/
     }
 
     boolean attackProtocol() throws GameActionException {
-        RobotInfo[] neabyRobots = rc.senseNearbyRobots(4);
-        for (RobotInfo rb : neabyRobots) {
-            if (rb.type == RobotType.ENLIGHTENMENT_CENTER && rc.canEmpower(2) && rb.team != rc.getTeam()){
-                System.out.println("Enemy EC found");
-                rc.empower(4);
-                return true;
-            }
-        }
-        RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius,rc.getTeam().opponent());
         if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
             rc.empower(actionRadius);
             return true;
         } else {
-            RobotInfo[] allenmt = rc.senseNearbyRobots(type.detectionRadiusSquared);
-            for (RobotInfo bot : allenmt) {
-                if (bot.team != rc.getTeam() && bot.type == RobotType.ENLIGHTENMENT_CENTER) {
-                    nav.navigate(bot.location);
+            for (RobotInfo bot : nearbyEnemys) {
+                if (bot.type == RobotType.ENLIGHTENMENT_CENTER) {
+                    enemyECloc = bot.location;
                     return true;
                 }
-            }if (enemyloc!=null){nav.navigate(enemyloc);}
-            for (RobotInfo bot : allenmt) {
+            }
+            for (RobotInfo bot : nearbyEnemys) {
                 if (bot.team != rc.getTeam()) {
                     nav.navigate(bot.location);
                     return true;
@@ -76,5 +63,16 @@ public class Politician extends Unit {
             }
         } else { return true; }
         return false;
+    }
+
+    void attack(MapLocation target, int maxDistanceSquared) throws GameActionException {
+        int distanceToTarget = myloc.distanceSquaredTo(target);
+        if (distanceToTarget <= maxDistanceSquared) {
+            if (rc.isReady() && rc.canEmpower(distanceToTarget)) {
+                rc.empower(distanceToTarget);
+            }
+        } else {
+            nav.navigate(target);
+        }
     }
 }
