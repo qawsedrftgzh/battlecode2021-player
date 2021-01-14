b1905886393975ca585c6de9539c9c71b99aec58
SkyNet214 <niklas20066@gmail.com>
2021-01-14 02:04:10 +0100

    tryMove() avoid passability under 0.2; added navigate2()

--- Navigation.java
+++ Navigation.java
@@ -5,6 +5,7 @@ import battlecode.common.*;
 public class Navigation {
     RobotController rc;
     Direction scoutDir;
+    double passabilityLimit = 0.2;
 
     public Navigation(RobotController r) {
         rc = r;
@@ -18,8 +19,8 @@ public class Navigation {
      * @return true if a move was performed
      * @throws GameActionException
      */
-   public boolean tryMove(Direction dir) throws GameActionException {
-        if (rc.canMove(dir)) {
+    public boolean tryMove(Direction dir) throws GameActionException {
+        if (rc.canMove(dir) && rc.sensePassability(rc.getLocation().add(dir)) > passabilityLimit) {
             rc.move(dir);
             return true;
         } else {
@@ -51,6 +52,20 @@ public class Navigation {
                 return false;
             }
         }
+    }
+
+    boolean navigate2(MapLocation dest) throws GameActionException {
+       MapLocation myloc = rc.getLocation();
+       if (myloc == dest) {
+           return true;
+       }
+       Direction dir = myloc.directionTo(dest);
+       if (tryMove(dir)) { return true; }
+       if (tryMove(dir.rotateRight())) { return true; }
+       if (tryMove(dir.rotateLeft())) { return true; }
+       if (tryMove(dir.rotateRight().rotateRight())) { return true; }
+       if (tryMove(dir.rotateLeft().rotateLeft())) { return true; }
+       return false;
     }
 
     boolean runaway(MapLocation loc) throws GameActionException {
