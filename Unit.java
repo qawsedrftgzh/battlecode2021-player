cb078714e31e155b3cdc6bdd16aded68691f7147
SkyNet214 <niklas20066@gmail.com>
2021-01-16 20:48:01 +0100

    recoded updateFlag() (unfinished, but no error)

--- Unit.java
+++ Unit.java
@@ -74,95 +74,94 @@ public class Unit extends Robot {
                 }
             }
         }
+        // --
+
+        // update neutral EC locations
+        if (neutralEClocs.size() > 0 || nearbyRobots.length > 0) {
+            if (neutralEClocs.size() > 0) {
+                neutralEClocs.sort(Comparator.comparingInt(x -> myloc.distanceSquaredTo(x))); // TODO check if it sorts right
+                for (MapLocation loc : neutralEClocs) {
+                    if (rc.canSenseLocation(loc)) {
+                        RobotInfo[] neutralAtLocation = rc.senseNearbyRobots(loc, 0, Team.NEUTRAL);
+                        if (neutralAtLocation.length <= 0) {
+                            neutralEClocs.remove(loc);
+                            RobotInfo[] teamAtLocation = rc.senseNearbyRobots(loc, 0, team);
+                            if (teamAtLocation.length > 0) {
+                                if (!teamEClocs.contains(loc)) {
+                                    teamEClocs.add(loc);
+                                }
+                            } else {
+                                if (!enemyEClocs.contains(loc)) {
+                                    enemyEClocs.add(loc);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (nearbyRobots.length > 0) {
+                for (RobotInfo b : nearbyRobots) {
+                    if (b.type == RobotType.ENLIGHTENMENT_CENTER && b.team == Team.NEUTRAL && !neutralEClocs.contains(b.location)) {
+                        neutralEClocs.add(b.location);
+                    }
+                }
+            }
+        }
+        // --
+
+        // update enemys EC locations
+        else if (enemyEClocs.size() > 0 || nearbyRobots.length > 0) {
+            if (enemyEClocs.size() > 0) {
+                enemyEClocs.sort(Comparator.comparingInt(x -> myloc.distanceSquaredTo(x))); // TODO check if it sorts right
+                for (MapLocation loc : enemyEClocs) {
+                    if (rc.canSenseLocation(loc)) {
+                        RobotInfo[] enemyAtLocation = rc.senseNearbyRobots(loc, 0, enemy);
+                        if (enemyAtLocation.length <= 0) {
+                            enemyEClocs.remove(loc);
+                            if (!teamEClocs.contains(loc)) {
+                                teamEClocs.add(loc);
+                            }
+                        }
+                    }
+                }
+            }
+            if (nearbyRobots.length > 0) {
+                for (RobotInfo b : nearbyRobots) {
+                    if (b.type == RobotType.ENLIGHTENMENT_CENTER && b.team == enemy && !enemyEClocs.contains(b.location)) {
+                        enemyEClocs.add(b.location);
+                    }
+                }
+            }
+        }
+        // --
+
+        // update team EC locations
+        else if (teamEClocs.size() > 0 || nearbyRobots.length > 0) {
+            if (teamEClocs.size() > 0) {
+                for (MapLocation loc : teamEClocs) {
+                    if (rc.canSenseLocation(loc)) {
+                        RobotInfo[] teamAtLocation = rc.senseNearbyRobots(loc, 0, team);
+                        if (teamAtLocation.length <= 0) {
+                            teamEClocs.remove(loc);
+                            if (!enemyEClocs.contains(loc)) {
+                                enemyEClocs.add(loc);
+                            }
+                        }
+                    }
+                }
+            }
+            if (nearbyRobots.length > 0) {
+                for (RobotInfo b : nearbyRobots) {
+                    if (b.type == RobotType.ENLIGHTENMENT_CENTER && b.team == team && !teamEClocs.contains(b.location)) {
+                        teamEClocs.add(b.location);
+                    }
+                }
+            }
+        }
+        // --
+
         // TODO send locations
 
 
-        // update neutral EC locations
-        if (neutralEClocs.size() > 0) {
-
-        }
-
-        // updating enemys EC locations
-        else if (enemyEClocs.size() > 0) {
-            enemyEClocs.sort(Comparator.comparingInt(a -> myloc.distanceSquaredTo(a))); // TODO check if it sorts right
-            for (MapLocation loc : enemyEClocs) {
-                if (rc.canSenseLocation(loc)) {
-
-                }
-            }
-        }
-        /** if (enemyECloc != null && myflag == 0) {
-            flags.sendLocationWithInfo(enemyECloc, InfoCodes.ENEMYEC);
-        }
-        if (enemyECloc != null && rc.canSenseLocation(enemyECloc)) {
-            RobotInfo[] enemyAtLocation = rc.senseNearbyRobots(enemyECloc, 0, enemy);
-            if (enemyAtLocation.length != 0) {
-                if (enemyAtLocation[0].type != RobotType.ENLIGHTENMENT_CENTER) {
-                    flags.sendLocationWithInfo(enemyECloc, InfoCodes.TEAMEC);
-                    enemyECloc = null;
-                }
-            } else {
-                RobotInfo[] teamAtLocation = rc.senseNearbyRobots(enemyECloc, 0, team);
-                if (teamAtLocation.length != 0) {
-                    flags.sendLocationWithInfo(enemyECloc, InfoCodes.TEAMEC);
-                    enemyECloc = null;
-                }
-            }
-        }
-        if (enemyECloc == null && nearbyEnemys.length != 0) {
-            for (RobotInfo b : nearbyEnemys) {
-                if (b.type == RobotType.ENLIGHTENMENT_CENTER) {
-                    if (!(nearbyTeam.length == 0)) {
-                        enemyECloc = b.location;
-                    }
-                    flags.sendLocationWithInfo(b.location, InfoCodes.ENEMYEC);
-                    break;
-                }
-            }
-        } **/
-        // --
-
-        // update neutral EC location
-        if (neutralECloc != null && myflag == 0) {
-            flags.sendLocationWithInfo(neutralECloc, InfoCodes.NEUTRALEC);
-        }
-        if (neutralECloc != null && rc.canSenseLocation(neutralECloc)) {
-            RobotInfo[] neutralAtLocation = rc.senseNearbyRobots(neutralECloc, 0, Team.NEUTRAL);
-            if (neutralAtLocation.length == 0) {
-                RobotInfo[] teamAtLocation = rc.senseNearbyRobots(neutralECloc, 0, team);
-                if (teamAtLocation.length > 0) {
-                    flags.sendLocationWithInfo(neutralECloc, InfoCodes.TEAMEC);
-                    neutralECloc = null;
-                } else {
-                    if (nearbyTeam.length > 0) {
-                        enemyECloc = neutralECloc;
-                    }
-                    flags.sendLocationWithInfo(neutralECloc, InfoCodes.ENEMYEC);
-                }
-            }
-        }
-        if (neutralECloc == null && nearbyNeutrals.length != 0) {
-            for (RobotInfo b : nearbyEnemys) {
-                if (b.type == RobotType.ENLIGHTENMENT_CENTER) {
-                    if (!(nearbyTeam.length == 0)) {
-                        enemyECloc = b.location;
-                    }
-                    flags.sendLocationWithInfo(b.location, InfoCodes.ENEMYEC);
-                    break;
-                }
-            }
-        }
-        /**
-        if (neutralECloc != null && rc.canSenseLocation(neutralECloc)) {
-            for (RobotInfo b : all) {
-                if (b.type == RobotType.ENLIGHTENMENT_CENTER && b.team == Team.NEUTRAL) {
-                    if (!(nearbyTeam.length == 0)) {
-                        neutralECloc = b.location;
-                    }
-                    flags.sendLocationWithInfo(b.location, InfoCodes.NEUTRALEC);
-                    break;
-                }
-            }
-        } **/
     }
 }
