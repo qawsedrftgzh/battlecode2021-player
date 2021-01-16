a413f5a07df19bc64d14792100c346e03991aee0
SkyNet214 <niklas20066@gmail.com>
2021-01-16 03:40:56 +0100

    added and edited infocodes

--- InfoCodes.java
+++ InfoCodes.java
@@ -1,8 +1,7 @@
 package battlecode2021;
 
 public class InfoCodes {
-    public static final int STARTATTACK = 1;
-    public static final int STOPATTACK = 2;
-    public static final int ENEMYS = 5;
-    public static final int ENEMYHQ = 7;
+    public static final int ENEMYEC = 1;
+    public static final int TEAMEC = 2;
+    public static final int NEUTRALEC = 3;
 }
