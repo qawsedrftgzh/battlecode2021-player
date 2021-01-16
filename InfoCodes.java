25e70782b0ce6a0d01e1ab3638aad8db7dc3a471
SkyNet214 <niklas20066@gmail.com>
2021-01-16 16:37:55 +0100

    added 3 new infocodes

--- InfoCodes.java
+++ InfoCodes.java
@@ -4,4 +4,7 @@ public class InfoCodes {
     public static final int ENEMYEC = 1;
     public static final int TEAMEC = 2;
     public static final int NEUTRALEC = 3;
+    public static final int ATTACKENEMY = 4;
+    public static final int ATTACKTEAM = 5;
+    public static final int ATTACKNEUTRAL = 6;
 }
