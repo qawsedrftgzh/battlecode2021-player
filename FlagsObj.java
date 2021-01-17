d9bed8ee748183b761e9459df90513b70d930638
SkyNet214 <niklas20066@gmail.com>
2021-01-17 04:44:29 +0100

    added FlagsObj class

---
+++ FlagsObj.java
@@ -0,0 +1,11 @@
+package battlecode2021;
+import battlecode.common.*;
+
+public class FlagsObj {
+    RobotInfo bot;
+    int lastflag;
+    public FlagsObj(RobotInfo bot, int lastflag) {
+        this.bot = bot;
+        this.lastflag = lastflag;
+    }
+}
