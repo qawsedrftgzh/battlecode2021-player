4d4e3051307bc9a11971fcc7e1581600e404c04e
SkyNet214 <niklas20066@gmail.com>
2021-01-17 18:05:11 +0100

    added convertToInfoCode() takes to teams and converts one to InfoCode

--- InfoCodes.java
+++ InfoCodes.java
@@ -1,8 +1,23 @@
 package battlecode2021;
 
+
+import battlecode.common.Team;
+
 public class InfoCodes {
     public static final int ENEMYEC = 1;
     public static final int TEAMEC = 2;
     public static final int NEUTRALEC = 3;
     public static final int ATTACKENEMY = 4;
+
+    public static int convertToInfoCode(Team team, Team myteam) {
+        if (team != null && myteam != null) {
+            if (team==myteam) {
+                return InfoCodes.TEAMEC;
+            } else if (team.opponent() == myteam) {
+                return InfoCodes.ENEMYEC;
+            } else {
+                return InfoCodes.NEUTRALEC;
+            }
+        } return 0;
+    }
 }
