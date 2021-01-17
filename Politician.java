2cf1980b5f77902d430ca52addce859a626f8113
SkyNet214 <niklas20066@gmail.com>
2021-01-17 18:06:16 +0100

    deleted unnecessary stuff; now attack neutral EC first

--- Politician.java
+++ Politician.java
@@ -1,5 +1,9 @@
 package battlecode2021;
 import battlecode.common.*;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Politician extends Unit {
     boolean superpol = false; //if the plotician has more then 200 influence
     public Politician(RobotController r) {
@@ -10,25 +14,17 @@ public class Politician extends Unit {
     public void takeTurn() throws  GameActionException{
         super.takeTurn();
         RobotInfo[] nearbyEnemys = rc.senseNearbyRobots(type.sensorRadiusSquared,enemy);
-        for (RobotInfo bot : rc.senseNearbyRobots()) {
-            if (bot.type == RobotType.ENLIGHTENMENT_CENTER && bot.team != team) {
-                enemyEClocs.add(bot.location);
-            }
-        }
-        if (nearbyEnemys.length == 1){
+        if (nearbyEnemys.length > 0){
+            Arrays.sort(nearbyEnemys, Comparator.comparingInt(x -> myloc.distanceSquaredTo(x.location)));
             RobotInfo bot = nearbyEnemys[0];
-            if (bot.location.distanceSquaredTo(rc.getLocation())<3){
-                tryEmpower(2);
-            }else {
-                nav.navigate(bot.location, false);
-            }
+            attack(bot.location, 2, false);
         }
         if (neutralEClocs.size() != 0){
-            attack(neutralEClocs.get(0), 1);
+            attack(getNearestLocation(neutralEClocs), 1, true);
         }
         if (enemyEClocs.size() != 0) {
             System.out.println("I am attacking a enemy EC");
-            attack(enemyEClocs.get(0), 1);
+            attack(getNearestLocation(enemyEClocs), 1, true);
         }
         if (nearbyEnemys.length > 0) {
             tryEmpower(actionRadius);
@@ -58,15 +54,13 @@ public class Politician extends Unit {
         return false;
     }
 
-    void attack(MapLocation target, int maxDistanceSquared) throws GameActionException {
+    void attack(MapLocation target, int maxDistanceSquared, boolean limitPassability) throws GameActionException {
         if (target != null) {
             int distanceToTarget = rc.getLocation().distanceSquaredTo(target);
             if (distanceToTarget <= maxDistanceSquared) {
-                if (rc.isReady() && rc.canEmpower(distanceToTarget)) {
-                    rc.empower(distanceToTarget);
-                }
+                tryEmpower(distanceToTarget);
             } else {
-                nav.navigate(target, true);
+                nav.navigate(target, limitPassability);
             }
         }nav.scout();
     }
